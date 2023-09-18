5d99fd70d19b586dfa3952b55e222dbc7025a7a6
Frida Sofía Andrade Sierra <frisoandrade@gmail.com>
2023-09-18 16:44:56 -0600

    api running

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -25,15 +25,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> getApiGolioth() async {
     try {
-      final Uri uri = Uri.parse(
-        "https://api.golioth.io/swagger.json");
+      final Uri uri = Uri.parse("https://api.golioth.io/v1/projects/sk-iot/devices?x-api-key=CjV9MziBnPZ3Ou4ZUEh4Fvo3DEEjTLPS");
       final response = await http.get(uri);
 
       if (response.statusCode == 200) {
         // Si mi solicitud fue exitosa, convierte la respuesta JSON a un objeto Golioth
         golioth = goliothFromJson(response.body);
+        print('Holis');
         print(golioth);
-        print('Ahuevito');
         setState(() {});
       } else {
         // Manejar errores de respuesta no exitosa aqu¨ª, por ejemplo, mostrar un mensaje de error
@@ -47,19 +46,45 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('API REST Golioth'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hello'),
-          ],
+    // Verificar si golioth no es nulo y tiene elementos en la lista
+    if (golioth != null && golioth!.list.isNotEmpty) {
+      final firstElement = golioth!.list?.first;
+      // Acceder al id y al nombre
+      final id = firstElement?.id;
+      final name = firstElement?.name;
+      
+      // Acceder al primer elemento de la lista (puedes ajustar esto seg¨²n tus necesidades)
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('API REST Golioth ID'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('API REST Golioth ID: $id'),
+              Text('Nombre: $name'),
+            ],
+          ),
+        ),
+      );
+    } else {
+      // Manejar el caso en el que golioth es nulo o la lista est¨¢ vac¨ªa
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('API REST Golioth - Sin datos disponibles'),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('No se encontraron datos disponibles.'),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
